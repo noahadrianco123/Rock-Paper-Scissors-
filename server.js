25f2e26cd47d591593636216a0c96be6223e8345
noahadrianco123 <noahadrianco26@gmail.com>
2026-02-20 13:48:39 +0800

    Refactor waiting messages for clarity

--- server.js
+++ server.js
@@ -14,32 +14,23 @@ let rooms = {};
 io.on('connection', (socket) => {
     socket.on('joinGame', ({ roomId, maxScore }) => {
         socket.join(roomId);
-        
-        // Create room if it doesn't exist and set the score limit
         if (!rooms[roomId]) {
             rooms[roomId] = { players: [], moves: {}, maxScore: maxScore || 3, rematchVotes: 0 };
         }
-        
         if (rooms[roomId].players.length < 2) {
             rooms[roomId].players.push(socket.id);
         }
-
         if (rooms[roomId].players.length === 2) {
-            // Tell both players the game has started and what the max score is
             io.to(roomId).emit('gameStart', rooms[roomId].maxScore);
         } else {
-            socket.emit('waiting', 'Waiting for opponent to join this link...');
+            socket.emit('waiting', 'Waiting for opponent to join...');
         }
     });
 
     socket.on('makeMove', ({ roomId, move }) => {
         if (!rooms[roomId]) return;
-        
         rooms[roomId].moves[socket.id] = move;
-
-        // Tell the OTHER player that this player has made a choice
         socket.to(roomId).emit('opponentMoved');
-
         if (Object.keys(rooms[roomId].moves).length === 2) {
             io.to(roomId).emit('roundResult', rooms[roomId].moves);
             rooms[roomId].moves = {}; 
@@ -49,15 +40,15 @@ io.on('connection', (socket) => {
     socket.on('rematch', (roomId) => {
         if (!rooms[roomId]) return;
         rooms[roomId].rematchVotes++;
-        
         if (rooms[roomId].rematchVotes === 2) {
-            rooms[roomId].rematchVotes = 0; // Reset votes
-            io.to(roomId).emit('gameStart', rooms[roomId].maxScore); // Restart game
+            rooms[roomId].rematchVotes = 0;
+            io.to(roomId).emit('gameStart', rooms[roomId].maxScore);
         } else {
-            socket.emit('waiting', 'Waiting for opponent to accept rematch...');
+            socket.emit('waiting', 'Waiting for opponent to accept...');
         }
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
