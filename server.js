48d038f5ad2624fb1debec29040fb099116d5a0e
noahadrianco123 <noahadrianco26@gmail.com>
2026-02-20 13:12:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 let rooms = {}; 
 
 io.on('connection', (socket) => {
-    socket.on('joinGame', (roomId) => {
+    socket.on('joinGame', ({ roomId, maxScore }) => {
         socket.join(roomId);
         
+        // Create room if it doesn't exist and set the score limit
         if (!rooms[roomId]) {
-            rooms[roomId] = { players: [], moves: {} };
+            rooms[roomId] = { players: [], moves: {}, maxScore: maxScore || 3, rematchVotes: 0 };
         }
         
         if (rooms[roomId].players.length < 2) {
@@ -24,7 +25,8 @@ io.on('connection', (socket) => {
         }
 
         if (rooms[roomId].players.length === 2) {
-            io.to(roomId).emit('gameStart');
+            // Tell both players the game has started and what the max score is
+            io.to(roomId).emit('gameStart', rooms[roomId].maxScore);
         } else {
             socket.emit('waiting', 'Waiting for opponent to join this link...');
         }
@@ -35,9 +37,24 @@ io.on('connection', (socket) => {
         
         rooms[roomId].moves[socket.id] = move;
 
+        // Tell the OTHER player that this player has made a choice
+        socket.to(roomId).emit('opponentMoved');
+
         if (Object.keys(rooms[roomId].moves).length === 2) {
             io.to(roomId).emit('roundResult', rooms[roomId].moves);
             rooms[roomId].moves = {}; 
+        }
+    });
+
+    socket.on('rematch', (roomId) => {
+        if (!rooms[roomId]) return;
+        rooms[roomId].rematchVotes++;
+        
+        if (rooms[roomId].rematchVotes === 2) {
+            rooms[roomId].rematchVotes = 0; // Reset votes
+            io.to(roomId).emit('gameStart', rooms[roomId].maxScore); // Restart game
+        } else {
+            socket.emit('waiting', 'Waiting for opponent to accept rematch...');
         }
     });
 });
